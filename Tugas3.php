1688c82b48e2e1a37237e03b40b5c543a4ca14bb
roykhan <roykhansyihab@gmail.com>
2023-11-23 11:53:11 +0700

    mengupdate

---
+++ Tugas3.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+        <h1 align="center" >POLITEKNIK BALEKKAMBANG</h1>
+        <h1 align="center" >NAMA MAHASISWA</h1>
+        <h1 align="center" >NAMA : ROYKHAN SYIHAB</h1>
+        <h1 align="center" >NIM : 2022010030</h1>
+        <h1 align="center" >MATA KULIAH : PRAKTE PEMEGRAMAN WEB 1</h1>
+    </p>   
+</body>
+</html>
